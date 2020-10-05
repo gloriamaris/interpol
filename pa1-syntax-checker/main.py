d1148b5c1f835aa1578283f6d6aa0a48a106f08c
Monique Dingding <monique.dingding@lamudi.com>
2020-10-05 22:27:09 +0800

    checks if the produced grammar is valid

--- pa1-syntax-checker/main.py
+++ pa1-syntax-checker/main.py
@@ -3,15 +3,21 @@ class SyntaxChecker:
     # ASCII printable characters will be checked through ord() 
     def __init__(self):
         self.lexicons = {
-            "markers": ['BEGIN', 'END'],
-            "display_methods": ['PRINT', 'PRINTLN'],
-            "operators": ['ADD', 'SUB', 'MUL', 'DIV']
+            "MARKER": ["BEGIN", "END"],
+            "DISPLAY": ["PRINT", "PRINTLN"],
+            "OPERATOR": ["ADD", "SUB", "MUL", "DIV", "MOD"]
+        }
+
+        self.grammar = {
+            "OPERATOR": "OP int int",
+            "DISPLAY": "DIS string",
+            "MARKER": "MARK"
         }
 
     # token     <string>
     # order     "start", "end"
     def check_if_marker(self, token, order):
-        return True if token in self.lexicons["markers"] else False
+        return True if token in self.lexicons["MARKER"] else False
 
     # Display welcome message
     def display_welcome_message(self):
@@ -22,18 +28,77 @@ class SyntaxChecker:
     def display_output(self, message_type):
         return "The syntax is " + message_type + "."
 
-    def proceed_execution(self, token):
-        return True if self.check_lexicon(token) else False
-    # Checks if valid lexicons
-    def check_lexicon(self, token):
-        is_start_marker = self.check_if_marker(token, 'start')
-        is_operator = True if token in self.lexicons["operators"] else False
-        is_display_method = True if token in self.lexicons["display_methods"] else False
+    def is_int(self, token):
+        try:
+            int(token)
+            return True
+        except ValueError:
+            return False
 
-        return True if (is_start_marker or is_operator or is_display_method) else False
+    # Lexicon checkers
+    def is_start_marker(self, token):
+        return self.check_if_marker(token, "start")
+
+    def is_operator(self, token):
+        return True if token in self.lexicons["OPERATOR"] else False
+    
+    def is_display_method(self, token):
+        return True if token in self.lexicons["DISPLAY"] else False
+
+    def is_number(self, token):
+        return True if (self.check_ascii(token) and self.is_int(token)) else False
+    
+    def is_string_literal(self, token): 
+        return True if (token[0] == '"' and token[len(token) - 1] == '"' and self.check_ascii(token)) else False
+    
+    def check_ascii(self, token):
+        return len(token) == len(token.encode())
+
+    # Lexicon checkers
+    def check_lexicon(self, token):
+        if (self.is_start_marker(token) == True):
+            return "MARK"
+        if (self.is_operator(token) == True):
+            return "OP"
+        if (self.is_display_method(token) == True):
+            return "DIS"
+        if (self.is_number(token) == True):
+            return "int"
+        if (self.is_string_literal(token) == True):
+            return "string"
+        
+        return False
+    
+    def check_syntax(self, line_of_code):
+        tokens = line_of_code.split()
+        index = 0
+        is_valid_syntax = True
+        is_valid_grammar = False
+        produced_grammar = []
+
+        print("tokens", tokens)
+
+        # generate the grammar produced from the line of code
+        for token in tokens:
+            keyword = self.check_lexicon(token)
+
+            if (keyword == False):
+                is_valid_syntax = False
+                break
+                
+            produced_grammar.append(keyword)
+
+        final_grammar = " ".join(produced_grammar) 
+        print(final_grammar)
+        
+        # compare to the list of valid grammars
+        for key, val in self.grammar.items():
+            if (val == final_grammar):
+                is_valid_grammar = True
+
+        return True if (is_valid_grammar and is_valid_syntax) else False
 
     def execute(self):
-        line = 0
         is_valid_program = True
         begin_message_displayed = False
         should_begin = False
@@ -42,19 +107,19 @@ class SyntaxChecker:
 
         while (is_valid_program):
             line_of_code = input()
-            if (line_of_code == 'BEGIN'):
+            if (line_of_code == "BEGIN"):
                 should_begin = True
             
-            if (should_begin and line_of_code == 'END'):
+            if (should_begin and line_of_code == "END"):
                 is_valid_program = False
 
             if (should_begin):
                 if (begin_message_displayed == False):
-                    message = self.display_output('correct')
+                    message = self.display_output("correct")
                     print(message + " Beginning syntax checker.")
                     begin_message_displayed = True
                 else:
-                    is_syntax_valid = self.proceed_execution(line_of_code)
+                    is_syntax_valid = self.check_syntax(line_of_code)
                     message_type = "correct" if is_syntax_valid else "incorrect"
                     print(self.display_output(message_type))
 
