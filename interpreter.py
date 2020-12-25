df4df49c92bb3fa3af3b3eb58a24ede5f6340445
Monique Dingding <monique.dingding@lamudi.com>
2020-12-25 17:04:54 +0800

    New file for interpreter ass #3

---
+++ interpreter.py
@@ -0,0 +1,11 @@
+
+# Main class for the program
+class Interpreter:
+  def execute(self):
+    is_valid_file = True
+    filename = ""
+
+    print("Enter INTERPOL file: ")
+    filename = input()
+
+
